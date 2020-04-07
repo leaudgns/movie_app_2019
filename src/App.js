1701c61538bdbe636d13b683032bd50e63009daa
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-07 16:35:54 +0900

    ReactJS 3.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
@@ -13,10 +12,20 @@ class App extends React.Component {
     // this.setState({ count: this.state.count - 1 });
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, Cruel world");
+  }
   render() {
+    console.log("I'm rendering");
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
+        <h1>The number is: {this.state.count}</h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
